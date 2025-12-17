f18bd931b1099251f2e1bc9d1a0574941e74dcd2
duyguaydiin <d.aydin9@hotmail.com>
2025-12-17 15:40:31 +0300

    Initial commit
    First commit I uploaded the files and some of the codes I wrote

---
+++ Main.java
@@ -0,0 +1,74 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+public class Main{
+    static final int MONTHS = 12;
+    static final int DAYS = 28;
+    static final int COMMS = 5;
+    static String[] commodities = {"Gold", "Oil", "Silver", "Wheat", "Copper"};
+    static String[] months = {"January","February","March","April","May","June",
+            "July","August","September","October","November","December"};
+    private static int[][][] profitData = null;
+    private static int getCommodityIndex(String commodityName) {
+        if (commodityName == null) return -1;
+        for (int i = 0; i < COMMS; i++) {
+            if (commodities[i].equals(commodityName)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+
+    public static void loadData() {
+
+    }
+
+    public static String mostProfitableCommodityInMonth(int month) {
+        return "DUMMY";
+    }
+
+    public static int totalProfitOnDay(int month, int day) {
+        return 1234;
+    }
+
+    public static int commodityProfitInRange(String commodity, int from, int to) {
+        return 1234;
+    }
+
+    public static int bestDayOfMonth(int month) {
+        return 1234;
+    }
+
+    public static String bestMonthForCommodity(String comm) {
+        return "DUMMY";
+    }
+
+    public static int consecutiveLossDays(String comm) {
+        return 1234;
+    }
+
+    public static int daysAboveThreshold(String comm, int threshold) {
+        return 1234;
+    }
+
+    public static int biggestDailySwing(int month) {
+        return 1234;
+    }
+
+    public static String compareTwoCommodities(String c1, String c2) {
+        return "DUMMY is better by 1234";
+    }
+
+    public static String bestWeekOfMonth(int month) {
+        return "DUMMY";
+    }
+
+    public static void main(String[] args) {
+        loadData();
+        System.out.println("Data loaded – ready for queries");
+    }
+}
